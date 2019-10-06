d6134e13bd7020a2a7c82a519ed2b1629729325b
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2019-10-06 10:48:51 +0000

    Added dogs view and inject HTML into view

--- wordcount/urls.py
+++ wordcount/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home),
-    path('dogs', views.home),
+    path('dogs', views.dogs),
 ]
--- wordcount/views.py
+++ wordcount/views.py
@@ -3,3 +3,7 @@ from django.http import HttpResponse
 
 def home(request):
     return HttpResponse("Hello World!")
+
+
+def dogs(request):
+    return HttpResponse("<h1>Dogs are great!</h1>")
