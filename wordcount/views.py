cddec6c5ea1ec4b6f5c42223648fa171988b2681
Sunny Hebbar <sunnyhebbar@hotmail.co.uk>
2019-10-06 11:07:29 +0000

    Created count view to render count.html page

--- wordcount/views.py
+++ wordcount/views.py
@@ -6,5 +6,5 @@ def home(request):
     return render(request, "home.html",)
 
 
-def dogs(request):
-    return HttpResponse("<h1>Dogs are great!</h1>")
+def count(request):
+    return render(request, "count.html")
